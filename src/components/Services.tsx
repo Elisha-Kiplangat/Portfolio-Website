a8a165542754b2a636d07d18878243a895623a9e
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-09-16 00:18:17 +0300

    text format

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,28 +1,27 @@
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode, faCloud, faPalette } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const services = [
   {
     icon: faCode,
     title: 'Vehicle Rental Management Application',
-    description: 'This is an online platform which allows users to rent/book a vehicle for their travels efficiently. It entails Payment service with stripe for customers to pay. The user can book a vehicle, on the other hand, the admin manages the all services',
+    description: 'A comprehensive fullstack solution for managing vehicle rentals, including vehicle listing, rental booking, customer management, and payment processing.',
   },
   {
     icon: faCloud,
     title: 'Restaurant API',
-    description: '',
+    description: 'A backend API designed to manage restaurant operations, including handling menus, orders, reservations, inventory, staff, and customer data. The API supports CRUD operations for all entities and provides functionality for real-time order updates and reporting.',
   },
   {
     icon: faPalette,
     title: 'Book Repository',
-    description: 'A fullstack application for managing books database',
+    description: 'A fullstack web application for managing a collection of books. Users can add, update, delete, and search for books, with features like user authentication and role-based access.',
   },
   {
     icon: faPalette,
     title: 'Todo App',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
+    description: 'A simple yet functional task management application that allows users to create, update, delete, and organize tasks. It supports features like task categorization, prioritization, and due date reminders, with a user-friendly interface to track progress.',
   },
 ];
 
@@ -30,30 +29,36 @@ const Services = () => {
   return (
     <section className="bg-purple-50 py-12 px-4 md:px-20 font-serif">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-4"> Projects</h2>
-        <p className="text-gray-600 mb-8"><b>Check it out!</b> Some of the the Outstanding projects i have been working on with the Skills and Experience i have in Software Development</p>
+        <h2 className="text-3xl font-bold mb-4">Projects</h2>
+        <p className="text-gray-600 mb-8">
+          <b>Check it out!</b> Some of the outstanding projects I have been working on with the skills and experience I have in Software Development
+        </p>
         <div className="border-b-2 border-purple-500 w-16 mx-auto mb-8"></div>
         <div className="flex flex-wrap justify-center">
           {services.map((service, index) => (
             <div 
               key={index} 
-              className="bg-blue-100 shadow-md rounded-lg p-6 m-4 w-64 transform transition-transform duration-300 hover:scale-105"
+              className="bg-blue-100 shadow-md rounded-lg p-6 m-4 w-64 transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between"
             >
-              <div className="w-12 h-12 mx-auto mb-4 text-purple-600">
-                <FontAwesomeIcon icon={service.icon} className="text-2xl" />
+              <div>
+                <div className="w-12 h-12 mx-auto mb-4 text-purple-600">
+                  <FontAwesomeIcon icon={service.icon} className="text-2xl" />
+                </div>
+                <h3 className="text-xl font-bold mb-2">{service.title}</h3>
+                <p className="text-gray-600 mb-4">{service.description}</p>
               </div>
-              <h3 className="text-xl font-bold mb-2">{service.title}</h3>
-              <p className="text-gray-600 mb-4">{service.description}</p>
-              <div className="border-b-2 border-purple-500 mx-auto mb-4"></div>
-              <div className="flex justify-around">
-                <button className="btn btn-outline border-2 btn-purple-500 text-purple-500 py-2 px-3 rounded-full hover:bg-purple-700 flex items-center">
-                  <FontAwesomeIcon icon={faGithub} className="text-2xl text-purple-800 hover:text-gray-900 transition-colors" />
-                  {/* <FontAwesomeIcon icon={faArrowRight} className="font-bold" /> */}
-                </button>
-                <button className='btn btn-outline border-2 btn-purple-500 text-purple-500 py-2 px-2 rounded-full hover:bg-purple-700 flex items-center'>
-                  <p className="text-purple-800 hover:text-gray-900 transition-colors">Link</p>
-                </button>
+              <div className=''>
+                <div className="border-b-2 border-purple-500 w-48 mx-auto mb-4"></div>
+                  <div className="flex justify-around mt-auto">
+                  <button className="btn btn-outline border-2 border-purple-500 text-purple-500 py-2 px-3 rounded-full flex items-center group-hover:bg-purple-500 group-hover:text-white transition-colors">
+                    <FontAwesomeIcon icon={faGithub} className="text-2xl group-hover:text-white transition-colors" />
+                  </button>
+                  <button className='btn btn-outline border-2 border-purple-500 text-purple-500 py-2 px-2 rounded-full flex items-center group-hover:bg-purple-500 group-hover:text-white transition-colors'>
+                    <p className="group-hover:text-white transition-colors">Link</p>
+                  </button>
+                </div>
               </div>
+
             </div>
           ))}
         </div>
--- src/components/WorkingProcess.tsx
+++ src/components/WorkingProcess.tsx
@@ -11,15 +11,16 @@ const WorkingProcess = () => {
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative z-10 text-white p-6 md:p-12">
         <div className="flex flex-col items-center">
+          <h2 className="text-4xl font-bold mb-4">Working Process</h2>
           <div className="mb-6">
             <button className="btn btn-outline border-4 border-blue-300 text-blue-300 rounded-full hover:bg-blue-500 hover:text-blue-300 flex items-center justify-center transition duration-300 ease-in-out">
                 <FontAwesomeIcon icon={faPlay} className="font-bold text-2xl" />
             </button>
           </div>
-          <h2 className="text-4xl font-bold mb-4">Working Process</h2>
           <p className="text-lg max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+           My approach to every project is rooted in a clear understanding of the requirements and a commitment to delivering high-quality solutions. I focus on innovative design, efficient problem-solving, and continuous improvement to produce results that not only meet expectations but exceed them.
           </p>
+
         </div>
       </div>
     </section>
