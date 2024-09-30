f621ad13e6bb55833f62d9f4fe8d07c1b24349cf
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-09-30 17:00:15 +0300

    social

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faTwitter, faWhatsapp, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faXTwitter, faWhatsapp, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { useState } from 'react';
 import emailjs from 'emailjs-com';
 import { toast, ToastContainer } from 'react-toastify';
@@ -144,17 +144,17 @@ const Contact = () => {
                 <a href="https://www.linkedin.com/in/elisha-kiplangat-111904260/" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faLinkedin} className="text-2xl text-blue-700 hover:text-blue-800 transition-colors" />
                 </a>
-                <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faTwitter} className="text-2xl text-blue-500 hover:text-blue-600 transition-colors" />
+                <a href="https://x.com/ElishaKiplangat" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faXTwitter} className="text-2xl text-gray-700 hover:text-gray-900 transition-colors" />
                 </a>
                 <a href="https://wa.me/+254700123762" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faWhatsapp} className="text-2xl text-green-500 hover:text-green-600 transition-colors" />
                 </a>
-                <a href="https://www.facebook.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.facebook.com/elisha.rono.908" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faFacebook} className="text-2xl text-blue-700 hover:text-blue-800 transition-colors" />
                 </a>
                 <a href="https://github.com/Elisha-Kiplangat" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faGithub} className="text-2xl text-gray-800 hover:text-gray-900 transition-colors" />
+                  <FontAwesomeIcon icon={faGithub} className="text-2xl text-gray-700 hover:text-gray-900 transition-colors" />
                 </a>
               </div>
             </div>
